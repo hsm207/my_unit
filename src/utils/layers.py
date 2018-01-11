fcea10b61b4bea8b07ae67d5dc431c7ddad8bb03
hsm207 <hsm207@users.noreply.github.com>
2018-01-11 11:11:44 -0500

    Created a layer that performs batch norm before applying the leaky ReLu activation function on a transposed convolution layer.

--- src/utils/layers.py
+++ src/utils/layers.py
@@ -3,7 +3,7 @@ This module contains layers that are used repeatedly in the models
 """
 import tensorflow as tf
 from tensorflow.python.keras.initializers import RandomNormal
-from tensorflow.python.keras.layers import Conv2D, BatchNormalization, LeakyReLU, Activation
+from tensorflow.python.keras.layers import Conv2D, BatchNormalization, LeakyReLU, Activation, Conv2DTranspose
 
 
 class LeakyReLUBNNSConv2d:
@@ -103,3 +103,40 @@ class GaussianVAE2D:
         samples = mu + sd * noise  # The multiplication is element-wise
 
         return samples, mu, sd
+
+
+class LeakyReLUBNNSConvTranspose2d:
+
+    def __init__(self, n_filters, kernel_size, stride, padding='valid', data_format='channels_first'):
+        """
+        This layer performs a transposed 2D convolution, batch normalization followed by leaky relu activation.
+
+        Note:
+
+            1. The transposed convolution has no activation function.
+            2. The batch norm layer has no scale parameter
+
+        :param n_filters: Number of filters for the convolution layer
+        :param kernel_size: The kernel size for the convolution layer
+        :param stride: The stride size for the convolution layer
+        :param padding: The padding type for the convolution layer
+        :param data_format: The data_format of the input tensor to be passed to this layer
+        """
+        bn_axis = 3 if data_format == 'channels_last' else 1
+        self.conv_transpose = Conv2DTranspose(filters=n_filters,
+                                              kernel_size=kernel_size,
+                                              strides=stride,
+                                              padding=padding,
+                                              activation='linear',
+                                              kernel_initializer=RandomNormal(mean=0, stddev=0.02),
+                                              data_format=data_format)
+        # TODO: Figure out why can leave out the scale parameter in batch norm if the following layer is a relu
+        self.batch_norm = BatchNormalization(axis=bn_axis, scale=False, center=True)
+        self.leaky_relu = LeakyReLU(alpha=1e-2)
+
+    def __call__(self, x):
+        x = self.conv_transpose(x)
+        x = self.batch_norm(x)
+        x = self.leaky_relu(x)
+
+        return x
