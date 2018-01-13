56c3e283b8e65e5927096da0f431b9941a266c51
hsm207 <hsm207@users.noreply.github.com>
2018-01-12 20:15:42 -0500

    Created a loss function to compute the cross entropy of a given batch of predictions.

--- src/utils/losses.py
+++ src/utils/losses.py
@@ -8,8 +8,8 @@ class MSE_Images():
     :param y_hat: A 4-D tensor representing the predictions where the first dimension is the batch size
     :return: A scalar representing the MSE between y and y_hat
     """
-    def __call__(self, y, y_hat):
 
+    def __call__(self, y, y_hat):
         # Get the number of images in the batch (assume both y and y_hat have the same batch size)
         n_y = y.shape[0].value
 
@@ -17,3 +17,20 @@ class MSE_Images():
         mse = sse / n_y
 
         return mse
+
+
+class Batch_Cross_Entropy():
+    def __call__(self, y, y_hat_logits):
+        """
+        Comptutes the cross entropy loss for a given batch of prediction logits  and class labels
+        :param y: A tensor of ints of shape (batch_size, ) representing the ground truth
+        :param y_hat_logits: A tensor of shape (batch_size, number_of_classes) representing the logits of each
+                             class prediction
+        :return: A scalar representing the cross entropy for the given batch
+        """
+        n_classes = y_hat_logits.get_shape()[1].value
+        y_ohe = tf.one_hot(y, n_classes)
+        loss_per_observation = tf.nn.softmax_cross_entropy_with_logits(labels=y_ohe, logits=y_hat_logits)
+        mean_loss = tf.reduce_mean(tf.reduce_mean(loss_per_observation))
+
+        return mean_loss
