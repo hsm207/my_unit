485bc071ed78d4d0db13c5049274ee047d147fb4
hsm207 <hsm207@users.noreply.github.com>
2018-01-15 21:51:18 -0500

    Updated training script.

--- src/cocogan_train_domain_adaptation.py
+++ src/cocogan_train_domain_adaptation.py
@@ -1,15 +1,13 @@
 import tensorflow as tf
-from tensorflow.python import debug as tf_debug
+from tensorflow.contrib.learn import RunConfig
 from tensorflow.python.keras.backend import set_learning_phase
 
 from utils.models import UNIT_DA_SHVN_TO_MNIST
 from utils.pipelines import train_input_fn_builder, test_input_fn_builder
 
-hooks = [tf_debug.LocalCLIDebugHook()]
-
 
 def model_fn(features, labels, mode, params):
-    model = UNIT_DA_SHVN_TO_MNIST(data_format='channels_last')
+    model = UNIT_DA_SHVN_TO_MNIST(data_format=params['data_format'], batch_size=params['batch_size'])
 
     if mode == tf.estimator.ModeKeys.PREDICT:
         set_learning_phase(False)
@@ -18,7 +16,10 @@ def model_fn(features, labels, mode, params):
 
         return tf.estimator.EstimatorSpec(
             mode=tf.estimator.ModeKeys.PREDICT,
-            predictions=predictions)
+            predictions=predictions,
+            export_outputs={
+                'classify': tf.estimator.export.PredictOutput(predictions)
+            })
 
     if mode == tf.estimator.ModeKeys.TRAIN:
         set_learning_phase(True)
@@ -29,8 +30,8 @@ def model_fn(features, labels, mode, params):
         discriminator_loss = model.update_discriminator(image_a, image_b, labels_a)
         generator_loss, _, _, _, _ = model.update_generator(image_a, image_b)
 
-        predictions_b = model.get_predictions(image_b)
-        metrics = model.get_metrics(labels_b, predictions_b['class'])
+        predictions_b = model.get_predictions(image_b)['class']
+        metrics = model.get_metrics(labels_b, predictions_b)
 
         total_loss = discriminator_loss + generator_loss
         train_op = model.get_train_op(loss_fn_discriminator=discriminator_loss,
@@ -61,27 +62,54 @@ def model_fn(features, labels, mode, params):
         )
 
 
-train_input_fn = train_input_fn_builder((65, 66), data_format='channels_last')
-test_input_fn = test_input_fn_builder(100, data_format='channels_last')
+# Define a dictionary of hyper parameters
+hyper_params = {
+    'data_format': 'channels_first',
+    'batch_size': 64,
+    'subset_train': (-1, -1),
+    'subset_test': -1,
+    'n_epochs': 100000
+}
 
+# Input pipelines
+train_input_fn = train_input_fn_builder(subset=hyper_params['subset_train'], data_format=hyper_params['data_format'],
+                                        batch_size=hyper_params['batch_size'], num_epochs=1)
+
+test_input_fn = test_input_fn_builder(subset=hyper_params['subset_test'], data_format=hyper_params['data_format'])
+
+# Configuration for the Estimator
+config = RunConfig(save_summary_steps=187500,
+                   save_checkpoints_steps=9375,
+                   keep_checkpoint_max=2,
+                   model_dir='../model_dir')
+
+# Create the Estimator
 da_model = tf.estimator.Estimator(
     model_fn=model_fn,
-    model_dir='./model_dir/'
+    config=config,
+    params=hyper_params
 )
 
-# da_model.train(train_input_fn, steps=2, hooks=hooks)
-da_model.train(train_input_fn, steps=2)
-# da_model.evaluate(test_input_fn)
-# sess = tf.InteractiveSession()
-#
-# model = UNIT_DA_SHVN_TO_MNIST(data_format='channels_last')
-# images, labels = train_input_fn()
-# img_a, img_b = images['image1'], images['image2']
-# labels_a, labels_b = labels['label1'], labels['label2']
-#
-# op1 = model.dis(img_a, img_b)
-# op1
-#
-# init = tf.global_variables_initializer()
-# sess.run(init)
-# sess.run(tf.shape(op1[1]), feed_dict={learning_phase(): 1})
+# Placeholder to build a Serving input function
+if hyper_params['data_format'] == 'channels_last':
+    img_serv = tf.placeholder(tf.float32, (64, 32, 32, 1))
+else:
+    img_serv = tf.placeholder(tf.float32, (64, 1, 32, 32))
+
+# Train the model and save the model that gives the best accuracy
+best_accuracy = 0
+
+for i in range(hyper_params['n_epochs']):
+    print('Epoch: {}'.format(i + 1))
+    da_model.train(train_input_fn)
+    res = da_model.evaluate(test_input_fn)
+
+    if res['classification_accuracy'] > best_accuracy:
+        best_accuracy = res['classification_accuracy']
+        print(f'best accuracy is {best_accuracy} and achieved on epoch {i+1}')
+
+        serv_input_fn = tf.estimator.export.build_raw_serving_input_receiver_fn({
+            'image': img_serv
+        }, default_batch_size=64)
+
+        da_model.export_savedmodel('../best_model', serv_input_fn)
